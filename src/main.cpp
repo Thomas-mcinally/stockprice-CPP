7ad3584e6e32672cb7af64fd895ad9f68cd154d7
Thomas-mcinally <95553476+Thomas-mcinally@users.noreply.github.com>
2024-03-19 15:14:58 +0000

    Release static binary (#6)
    * print libcurl version
    
    * release on PR to see if get good binary
    
    * only re-release macos
    
    * using httplib
    
    * update cmakelists.txt
    
    * add OpenSSL
    
    * first working build
    
    * try build x84)
    
    * clean openssl directory and remove no-asm
    
    * try to build
    
    * update release flow
    
    * improve build

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "../include/httplib/httplib.h"
+
+
+int main(int argc, char** argv) {
+
+
+    httplib::Client cli("https://query2.finance.yahoo.com");
+    cli.enable_server_certificate_verification(false); // Want to distribute as static binary
+    auto res = cli.Get("/v8/finance/chart/BTC-USD?interval=1d&range=30d");
+
+    printf("Status code: %d\n", res->status);
+    return 0;
+}
