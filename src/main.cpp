ea4cb49b248b5d24fecb0c8a5ea7a5bd7320a137
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-03-19 20:46:37 +0000

    ignore openssl library for languages breakdown

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 
 
 int main(int argc, char** argv) {
-
-
     httplib::Client cli("https://query2.finance.yahoo.com");
     cli.enable_server_certificate_verification(false); // Want to distribute as static binary
     auto res = cli.Get("/v8/finance/chart/BTC-USD?interval=1d&range=30d");
