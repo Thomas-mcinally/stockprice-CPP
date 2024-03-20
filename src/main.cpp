76c3a9d77000c97151dbb09939127d0fcd8413cc
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-03-20 08:01:52 +0000

    print result for multiple tickers

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,33 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "../include/httplib/httplib.h"
-
+#include <sstream>
+#include <vector>
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        printf("Please provide one stock ticker\n");
-        printf("Example: stockprice tsla\n");
+        printf("Please provide o comman-delimited list of tickets\n");
+        printf("Example: stockprice tsla,btc-usd\n");
         return 0;
     }
-    std::string ticker = argv[1];
+
+    std::stringstream s_stream(argv[1]); 
+    std::vector<std::string> tickers;
+    std::string ticker;
+
+    while(std::getline(s_stream, ticker, ',')) { 
+        tickers.push_back(ticker);
+    }
 
     httplib::Client cli("https://query2.finance.yahoo.com");
     cli.enable_server_certificate_verification(false); // Want to distribute as static binary
-    std::string url = "/v8/finance/chart/" + ticker + "?interval=1d&range=30d";
-    
-    auto res = cli.Get(url);
 
-    printf("Status code: %d\n", res->status);
-    printf("Body: %s\n", res->body.c_str());
+    for(const auto& ticker : tickers) {
+        std::string url = "/v8/finance/chart/" + ticker + "?interval=1d&range=30d";
+        auto res = cli.Get(url.c_str());
+
+        printf("Status code: %d\n", res->status);
+        printf("Body: %s\n", res->body.c_str());
+    }
+
     return 0;
 }
