c7435820369fa7e54c330afb91c80908fd522c37
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-03-20 07:50:21 +0000

    input single ticket and print it

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 
 
 int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Please provide one stock ticker\n");
+        printf("Example: stockprice tsla\n");
+        return 0;
+    }
+    std::string ticker = argv[1];
+    printf("Ticker provided: %s\n", ticker.c_str());
+
     httplib::Client cli("https://query2.finance.yahoo.com");
     cli.enable_server_certificate_verification(false); // Want to distribute as static binary
     auto res = cli.Get("/v8/finance/chart/BTC-USD?interval=1d&range=30d");
 
     printf("Status code: %d\n", res->status);
+    printf("Body: %s\n", res->body.c_str());
     return 0;
 }
