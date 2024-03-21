b7b934dead4cfcc36e63822b09ac2523390b9005
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2024-03-21 07:34:39 +0000

    refactor implementation

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,30 @@ double calculate_percentage_price_change_over_n_days(int n, std::vector<int> tim
     return percentage_change;
 }
 
+void calculate_and_print_ticker_statistics(const std::string& ticker) {
+    httplib::Client cli("https://query2.finance.yahoo.com");
+    cli.enable_server_certificate_verification(false); // Want to distribute as static binary
+    std::string path = "/v8/finance/chart/" + ticker + "?interval=1d&range=30d";
+    auto res = cli.Get(path.c_str());
+    if (res->status == 404){
+        printf("The ticker %s is not listed on Yahoo Finance.\n", ticker.c_str());
+        return;
+    }
+
+    nlohmann::json response_json = nlohmann::json::parse(res->body);
+    std::string currency = response_json["chart"]["result"][0]["meta"]["currency"];
+    std::vector<double> closing_prices = response_json["chart"]["result"][0]["indicators"]["quote"][0]["close"];
+    std::vector<int> timestamps = response_json["chart"]["result"][0]["timestamp"];
+
+    double current_price = closing_prices.back();
+
+    double percentage_change_30day = calculate_percentage_price_change_over_n_days(30, timestamps, closing_prices);
+    double percentage_change_7day = calculate_percentage_price_change_over_n_days(7, timestamps, closing_prices);
+    double percentage_change_1day = calculate_percentage_price_change_over_n_days(1, timestamps, closing_prices);
+
+    printf("%s -- Current price: %.2f %s -- Daily change: %.2f%%, 7-day change: %.2f%%, 30-day change: %.2f%%\n", ticker.c_str(), current_price, currency.c_str(), percentage_change_1day, percentage_change_7day, percentage_change_30day);
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         printf("Please provide a comma-delimited list of tickers\n");
@@ -43,29 +67,8 @@ int main(int argc, char** argv) {
         tickers.push_back(ticker);
     }
 
-    httplib::Client cli("https://query2.finance.yahoo.com");
-    cli.enable_server_certificate_verification(false); // Want to distribute as static binary
-
-    for(std::string &ticker : tickers) {
-        std::string path = "/v8/finance/chart/" + ticker + "?interval=1d&range=30d";
-        auto res = cli.Get(path);
-        if (res->status == 404){
-            printf("The ticker %s is not listed on Yahoo Finance.\n", ticker.c_str());
-            continue;
-        }
-
-        nlohmann::json response_json = nlohmann::json::parse(res->body);
-        std::string currency = response_json["chart"]["result"][0]["meta"]["currency"];
-        std::vector<double> closing_prices = response_json["chart"]["result"][0]["indicators"]["quote"][0]["close"];
-        std::vector<int> timestamps = response_json["chart"]["result"][0]["timestamp"];
-
-        double current_price = closing_prices.back();
-
-        double percentage_change_30day = calculate_percentage_price_change_over_n_days(30, timestamps, closing_prices);
-        double percentage_change_7day = calculate_percentage_price_change_over_n_days(7, timestamps, closing_prices);
-        double percentage_change_1day = calculate_percentage_price_change_over_n_days(1, timestamps, closing_prices);
-
-        printf("%s -- Current price: %.2f %s -- Daily change: %.2f%%, 7-day change: %.2f%%, 30-day change: %.2f%%\n", ticker.c_str(), current_price, currency.c_str(), percentage_change_1day, percentage_change_7day, percentage_change_30day);
+    for(const std::string &ticker : tickers) {
+        calculate_and_print_ticker_statistics(ticker);
     }
 
     return 0;
